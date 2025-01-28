acc47cb5c07533b06c1250e3189e588062ce728d
Shamol Mojumder <shamolmojumder14@gmail.com>
2025-01-28 11:06:16 +0600

    Login Register Ecommerce Mern App Mern Stack Project

--- client/src/App.js
+++ client/src/App.js
@@ -6,12 +6,14 @@ import Contact from './pages/Contact';
 import Policy from './pages/Policy';
 import Pagenotfound from './pages/Pagenotfound';
 import Register from './pages/Auth/Register';
+import Login from './pages/Auth/Login';
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<Homepage />} />
         <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
         <Route path='/About' element={<About />} />
         <Route path='/Contact' element={<Contact />} />
         <Route path='/Policy' element={<Policy />} />
