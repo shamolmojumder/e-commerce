ea8bff7da3ee77e02fe39448bab761b4b33cfbde
Shamol Mojumder <shamolmojumder14@gmail.com>
2025-02-09 10:42:05 +0600

    Admin and User Panel Ecommerce Mern App Mern Stack Project 12:30

--- client/src/App.js
+++ client/src/App.js
@@ -9,13 +9,18 @@ import Login from "./pages/Auth/Login";
 import Dashboard from "./pages/user/Dashboard";
 import PrivateRoute from "./components/Routes/Private";
 import ForgotPasssword from "./pages/Auth/ForgotPasssword";
+import AdminRoute from "./components/Routes/AdminRoute";
+import AdminDashboard from "./pages/Admin/AdminDashboard";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/dashboard" element={<PrivateRoute />}>
-          <Route path="" element={<Dashboard />} />
+          <Route path="user" element={<Dashboard />} />
+        </Route>
+        <Route path="/dashboard" element={<AdminRoute />}>
+          <Route path="admin" element={<AdminDashboard />} />
         </Route>
         <Route path="/register" element={<Register />} />
         <Route path="/forgot-password" element={<ForgotPasssword />} />
---
+++ client/src/components/Layout/AdminMenu.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const AdminMenu = () => {
+    return (
+        <>
+            <div className="text-center">
+                <div class="list-group">
+                    <div>
+                        <h4>Admin Dashboard</h4>
+                        <NavLink to="/dashboard/admin/create-category" className="list-group-item list-group-item-action">Create category</NavLink>
+                        <NavLink to="/dashboard/admin/create-product" className="list-group-item list-group-item-action">Create product</NavLink>
+                        <NavLink to="/dashboard/admin/users" className="list-group-item list-group-item-action">Users</NavLink>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default AdminMenu
